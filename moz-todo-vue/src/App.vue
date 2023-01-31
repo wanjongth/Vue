feafe926a8998ad93787c28e3d18705f35ffa31a
wanjongth <infinity4869@gmail.com>
2023-01-31 18:45:39 +0900

    vue components rendering, events, methods and models

---
+++ moz-todo-vue/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <h1>My To-Do List</h1>
+    <!-- <to-do-form></to-do-form> -->
+    <to-do-form @todo-added="addToDo"></to-do-form>
+    <!-- <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item label="My ToDo Item" :done="true"></to-do-item>
+      </li>
+    </ul> -->
+    <!-- <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item :label="item.label" :done="item.done"></to-do-item>
+      </li>
+    </ul> -->
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item
+          :label="item.label"
+          :done="item.done"
+          :id="item.id"></to-do-item>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ToDoForm from "./components/ToDoForm";
+import ToDoItem from "./components/ToDoItem.vue";
+import uniqueId from "lodash.uniqueid";
+
+export default {
+  name: "app",
+  components: {
+    ToDoItem,
+    ToDoForm,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId("todo-"), label: "Learn Vue", done: false },
+        {
+          id: uniqueId("todo-"),
+          label: "Create a Vue project with the CLI",
+          done: true,
+        },
+        { id: uniqueId("todo-"), label: "Have fun", done: true },
+        { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
+      ],
+    };
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      console.log('To-do added:', toDoLabel);
+      this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false});
+    }
+}
+
+};
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
